22667cfaa0ab5b8423dce15b5e28a7772eb10c37
G4meOv3r <boyarnikov.sasha@gmail.com>
2020-09-07 22:39:57 +0600

    http error

---
+++ src/helpers/error.js
@@ -0,0 +1,17 @@
+const error = (error, message) => {
+    let name = "";
+    switch (error) {
+        case 400:
+            name = "Bad Request";
+            break;
+        case 401:
+            name = "Unauthorized";
+            break;
+        case 500:
+            name = "Internal Server Error";
+            break;
+    }
+    return { error, code, message };
+}
+
+export default error;
