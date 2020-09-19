e7ed2d2e299f7a951c2c96c583ff6926eaddc110
G4meOv3r <boyarnikov.sasha@gmail.com>
2020-09-19 22:37:26 +0600

    auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import authMiddleware from "../middlewares/auth.js";
+import authMiddleware from "../middlewares/auth/auth.js";
 
 import controller from "../controllers/auth/auth";
 
 const router = express.Router();
 router.post("/signup", controller.signup.post);
 router.post("/signin", controller.signin.post);
-router.post("/signout", authMiddleware.catching, controller.signout.post);
+router.post("/signout", authMiddleware.user.catching, controller.signout.post);
 
 export default router;
