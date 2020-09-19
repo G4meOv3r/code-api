d7bc039935906cb5f07733bb95997367117e84f9
G4meOv3r <boyarnikov.sasha@gmail.com>
2020-09-19 22:37:57 +0600

    profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import authMiddleware from "../middlewares/auth.js";
+import authMiddleware from "../middlewares/auth/auth.js";
 
 import controller from "../controllers/profile/profile";
 
 const router = express.Router();
-router.get('', authMiddleware.through, controller.profile.get);
+router.get('', authMiddleware.user.through, controller.profile.get);
 
 export default router;
